5db0842f5812e0897d8b836c9983cd06d7dc39e6
NGoyal22 <40000763+NGoyal22@users.noreply.github.com>
2018-07-08 01:11:42 +0530

    Create Predicting_Stock_Prices_Using_SVM.py

---
+++ Predicting_Stock_Prices_Using_SVM.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sklearn.svm import SVR
+import csv
+import matplotlib.pyplot as plt
+
+dates=[]
+prices=[]
+def get_data(filename):
+    with open(filename,'r') as csvfile:
+      csvfr=csv.reader(csvfile)
+      next(csvfr)
+      for row in csvfr:
+          dates.append(int(row[0].split('/')[1]))
+          prices.append(float(row[1]))
+    return
+get_data('HistoricalQuotes.csv')
+
+def predict_prices(dates,prices,x):
+    dates = np.reshape(dates,(len(dates),1))
+    
+    svr_len = SVR(kernel='linear',C=1e3)
+    svr_poly = SVR(kernel='poly',C=1e3,degree=2)
+    svr_rbf = SVR(kernel='rbf',C=1e3,gamma=0.1)
+    
+    svr_len.fit(dates,prices)
+    svr_rbf.fit(dates,prices)
+    svr_poly.fit(dates,prices)
+    
+    plt.scatter(dates,prices,color='black',label='Data')
+    plt.plot(dates,svr_len.predict(dates),color='green',label='linear model')
+    plt.plot(dates,svr_poly.predict(dates),color='blue',label='poly model')
+    plt.plot(dates,svr_rbf.predict(dates),color='red',label='rbf model')
+    plt.xlabel('Data')
+    plt.ylabel('Prices')
+    plt.title('Support Vector Machine')
+    plt.legend()
+    plt.show()
+    
+    return svr_rbf.predict(x)[0],svr_poly.predict(x)[0],svr_len.predict(x)[0]
+predict_price = predict_prices(dates,prices,8)
+print(predict_price)
