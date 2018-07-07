a7a2d87a10a60af0d4fac0071cdfbc23c9828c2a
NGoyal22 <40000763+NGoyal22@users.noreply.github.com>
2018-07-07 21:13:00 +0530

    Create Twitter_Sentiment_Analyzer.py

---
+++ Twitter_Sentiment_Analyzer.py
@@ -0,0 +1,20 @@
+import tweepy
+from textblob import TextBlob
+
+Consumer_Key ='_____'
+Consumer_Secret = '_____'
+Access_Token = '_____'
+Access_Token_Secret = '_____'
+
+auth = tweepy.OAuthHandler(Consumer_Key,Consumer_Secret)
+auth.set_access_token(Access_Token,Access_Token_Secret)
+
+api=tweepy.API(auth)
+tweets=str(input("Enter string whose you want tweets"+"\n"))
+print("You Entered String is :"+ tweets+"\n")
+public_tweets=api.search(tweets)
+
+for tweets in public_tweets:
+	print(tweets.text)
+	analysis=TextBlob(tweets.text)
+	print(analysis.sentiment)
